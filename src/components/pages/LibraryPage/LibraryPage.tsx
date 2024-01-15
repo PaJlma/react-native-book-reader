241b11da0ba29853e60f650ef5f85d8a314ce4ac
PaJlma <palma21042005@gmail.com>
2024-01-15 21:56:03 +0300

    created LibraryScreen component

---
+++ src/components/pages/LibraryPage/LibraryPage.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+import { View } from "react-native";
+import { LibraryPageProps } from "./LibraryPage.types";
+
+const LibraryPage: FC<LibraryPageProps> = (props) => {
+  return <View></View>;
+};
+
+export default LibraryPage;
