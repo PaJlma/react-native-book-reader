d32abd9fcdaba322fd916ac9a57622c65151a680
PaJlma <palma21042005@gmail.com>
2024-01-15 21:58:18 +0300

    pluged AppRouter

--- App.tsx
+++ App.tsx
@@ -1,22 +1,9 @@
 import { FC } from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { AppProps } from "./App.types";
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+import AppRouter from "@/AppRouter/AppRouter";
 
 const App: FC<AppProps> = (props) => {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+  return <AppRouter />;
 };
 
 export default App;
