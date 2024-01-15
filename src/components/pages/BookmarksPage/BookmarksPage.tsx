7a1139adcf2e9e64c0e1a6a061fabf3f00e3db30
PaJlma <palma21042005@gmail.com>
2024-01-15 21:56:37 +0300

    created BookmarksScreen component

---
+++ src/components/pages/BookmarksPage/BookmarksPage.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+import { View } from "react-native";
+import { BookmarksPageProps } from "./BookmarksPage.types";
+
+const BookmarksPage: FC<BookmarksPageProps> = (props) => {
+  return <View></View>;
+};
+
+export default BookmarksPage;
