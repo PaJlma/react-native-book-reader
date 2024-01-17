25c649e43da25fb827e2c9acd94913fb4ecc68ee
PaJlma <palma21042005@gmail.com>
2024-01-17 15:57:57 +0300

    some changes in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   root: true,
+  env: { browser: false, es2020: true },
   extends: ["plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/no-unused-vars": "off",
